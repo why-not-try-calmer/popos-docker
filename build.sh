63e5de6426510d1bb40407183fa9d0d70fb7b551
why-not-try-calmer <mrnycticorax@gmail.com>
2024-01-12 19:38:37 +0100

    added dispatch trigger

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash env
+#!/usr/bin/bash
 iso_url=https://iso.pop-os.org/22.04/amd64/intel/37/pop-os_22.04_amd64_intel_37.iso
 iso_name=popos.iso
 dockerhub_img=nycticoracs/pop_os:latest
@@ -8,5 +8,5 @@ wget -O $iso_name $iso_url
 sudo mount -o loop $iso_name rootfs
 fs=$(find . -type f | grep filesystem.squashfs)
 sudo unsquashfs -f -d unsquashfs/ $fs
-sudo tar -C unsquashfs -c . | docker import - $dockerhub_dst
+sudo tar -C unsquashfs -c . | docker import - $dockerhub_img
 docker push $dockerhub_img
--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash env
+#!/usr/bin/bash
 dockerhub_img=nycticoracs/pop_os:latest
 
 docker push $dockerhub_img
